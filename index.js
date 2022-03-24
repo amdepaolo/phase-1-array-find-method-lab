4a8d29df9bbc166e63e2ff81e9b14ff85baefaf1
Adam DePaolo <amdepaolo@gmail.com>
2022-03-23 20:22:30 -0400

    commit tree b6e0cc5e924b3b1fed9f5fe2858896b5c79d6c29

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// code your solution here  
+const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ];
+
+function didTheyWin(object){
+    return (object.result === "W")
+};
+
+function superbowlWin(array){
+    let win = array.find(didTheyWin)
+    if (!!win === true){
+        return win.year
+    }
+    else return undefined
+}
